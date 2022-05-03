1975ee194984835cd4e6b1d9ba264ec9e1f1f217
jhonatasv <jhonatasvieira42@gmail.com>
2022-05-03 16:41:11 -0300

    correção de datas

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,9 @@
-// verificar.addEventListener('click', function verificar(){
-//     const data = new Date()
-//     let ano = data.getFullYear()
-//     let birthday = document.querySelector('#birthday').value
-//     let result = document.querySelector('#result')
-    
-    
-//     if(birthday > ano){ 
-//         result.innerHTML = alert(`[Erro], Verifique os Dados`)
-//         result.innerHTML = `<p>Você veio do futuro? Verifique a Data de Nascimento</p>`
-   
-//     } else if ( birthday.length == 0) {
-//         result.innerHTML = `<p>Ops.. você esqueceu de Preenche o Ano de Nascimento</p>`
-   
-//     } else {
-//         let sexo = document.getElementsByName('fsexo')
-//         let idade = ano - Number(birthday)
-//         let genero = ''
-//         let img = document.createElement('img')
-//         img.setAttribute('id', 'foto')
-
-//         if(sexo[0].checked) {
-//             genero = 'Homem'
-//             if (idade >= 0 && idade < 2) { 
-//                 img.setAttribute('src', 'assets/img/bebe.m.jpg')
-//             } else if(idade < 12) {
-//                 img.setAttribute('src', 'assets/img/crianca.m.jpg')
-//             } else if(idade < 24) {
-//                 img.setAttribute('src', 'assets/img/adolescente.m.jpg')
-//             } else if(idade <= 59) {
-//                 img.setAttribute('src', 'assets/img/adulto.m.png')
-//             } else {
-//                 img.setAttribute('src', 'assets/img/idoso.m.jpg')
-//             }
-
-//         } else if(sexo[1]) {
-//             genero ='Mulher'
-
-//             if(idade >= 0 && idade < 2) {
-//                 img.setAttribute('src', 'assets/img/bebe.f.jpg')
-//             } else if(idade < 12) {
-//                 img.setAttribute('src', 'assets/img/crianca.f.jpg')
-//             } else if(idade < 24) {
-//                 img.setAttribute('src', 'assets/img/adolescente.f.jpg')
-//             } else if(idade <= 59) {
-//                 img.setAttribute('src', 'assets/img/adulto.f.jpg')
-//             } else {
-//                 img.setAttribute('src', 'assets/img/idosa.f.jpg')
-//             }
- 
-//         }
-
-//         result.innerHTML = `<p>Detectamos um ${genero} com ${idade} anos.</p>`
-//         result.appendChild(img)
-//         result.style.textAlign = 'center'
-//         document.querySelector('#foto').style.margin = '15px'
-//     }
-// })
-
 verificar.addEventListener('click', function verificar(){
     const data = new Date()
     let ano = data.getFullYear()
     let birthday = document.querySelector('#birthday').value
     let result = document.querySelector('#result')
-    
+    let idade = ano - Number(birthday)
     
     if(birthday > ano){ 
         result.innerHTML = alert(`[Erro], Verifique os Dados`)
@@ -71,12 +12,11 @@ verificar.addEventListener('click', function verificar(){
     } else if (birthday.length == 0) {
         result.innerHTML = `<p>Ops.. você esqueceu de Preenche o Ano de Nascimento</p>`
 
-    } else if(birthday.length > 5) {
+    } else if(birthday.length > 5 || idade > 121) {
         result.innerHTML = `<p>Data invalida. Tente Novamente</p>`
 
     } else {
         let sexo = document.getElementsByName('fsexo')
-        let idade = ano - Number(birthday)
         let genero = ''
         let img = document.createElement('img')
         img.setAttribute('id', 'foto')
@@ -111,7 +51,7 @@ verificar.addEventListener('click', function verificar(){
             }
 
         }
-        
+
         result.innerHTML = `<p>Detectamos um ${genero} com ${idade} anos.</p>`
         result.appendChild(img)
         result.style.textAlign = 'center'
